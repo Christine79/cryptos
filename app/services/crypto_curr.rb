3cfcb2f722a7a34bd9540a007a761157a7da0718
christine <christinejaouen@hotmail.com>
2018-11-08 19:38:56 +0100

    commit DEUX

--- app/services/crypto_curr.rb
+++ app/services/crypto_curr.rb
@@ -1,28 +1,47 @@
 class CryptoCurr
   require 'nokogiri'
   require 'open-uri'
-  def crypto_name
+  def init
+      @page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
       array_name=[]
-      page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-      page.css("td.currency-name").each do |x|
-      array_name << x["data-sort"]
-    end
-    array_name
-  end
-  currency_list = ['name', 'value']
-  #puts crypto_name
-  def crypto_value
+      @page.css("td.currency-name").each do |x|
+       array_name << x["data-sort"]
+      end
+      array_name
       array_value=[]
-      page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-      page.css("a.price").each do |y|
+      @page.css("a.price").each do |y|
           array_value << y["data-usd"]
       end
       array_value
-  end
-  #puts crypto_value
-  # puts Hash[currency_list.name, currency_list.value]
+#      puts Hash[crypto_name.zip(crypto_value)]
+      pp Hash[array_name.zip(array_value)]
+      kikihash = Hash[array_name.zip(array_value)]
+      kikihash.each do |k|
+          pp "voici: #{k}"
+          @crypto = Crypto.new(crypto_params)
 
-  currency_list.each do |curr|
-    puts "voici: !!!  #{curr.name}   #{curr.value} toc! "
+          respond_to do |format|
+            if @crypto.save
+              format.html { redirect_to @crypto, notice: 'crypto was successfully created.' }
+              format.json { render :show, status: :created, location: @crypto }
+            else
+              format.html { render :new }
+              format.json { render json: @crypto.errors, status: :unprocessable_entity }
+            end
+          end
+
+      end
+
+  end
+
+  #currency_list = [“id_name”, “name”]
+  #puts crypto_name
+  def perform
+  end
+
+  #puts crypto_value
+  def save
+
   end
 end
--- app/services/crypto_curri.rb
+++ app/services/crypto_curri.rb
@@ -2,13 +2,7 @@ class CryptoCurri
 def init
     require 'nokogiri'
     require 'open-uri'
-    page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-    page.css("td.currency-name").each do |x|
-      crypto.name << x["data-sort"]
-    end
-    array_name
-    page.css("a.price").each do |y|
-        crypto.value << y["data-usd"]
-    end
+    array_name=[]
+    @page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 end
 end
