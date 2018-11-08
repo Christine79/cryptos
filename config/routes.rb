9638f4d04aff12a1af7f2551212186baf46924c4
christine <christinejaouen@hotmail.com>
2018-11-08 20:15:30 +0100

    commit TROIS

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     resources :cryptos
 
     get '/static_page/home', to: 'static_page#home'
-    get '/static_page/initialize', to: 'static_page#initialize'
+    get '/static_page/init', to: 'static_page#init'
     get '/static_page/perform', to: 'static_page#perform'
     get '/static_page/perform', to: 'static_page#save'
 end
